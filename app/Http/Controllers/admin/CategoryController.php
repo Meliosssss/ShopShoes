b77921325748834c3ed8003b00df74ce6ebdde75
Meliosssss <phamhongquan19122001@gmail.com>
2024-04-07 02:49:32 +0700

    first commit

---
+++ app/Http/Controllers/admin/CategoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\TempImage;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
+
+class CategoryController extends Controller
+{
+    public function index(Request $request)
+    {
+        $categories =  Category::latest();
+        if (!empty($request->get('keyword'))) {
+            $categories =  $categories->where('name', 'like', '%' . $request->get('keyword') . '%');
+        }
+        $categories =  $categories->paginate(10);
+        return view('admin.category.list', compact('categories'));
+    }
+
+    public function create()
+    {
+        return view('admin.category.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'slug' => 'required|unique:categories',
+        ]);
+
+        if ($validator->passes()) {
+            $category = new Category();
+            $category->name = $request->name;
+            $category->slug = $request->slug;
+            $category->status = $request->status;
+            $category->save();
+            if (!empty($request->image_id)) {
+                $tempImage = TempImage::find($request->image_id);
+                $extArray = explode('.', $tempImage->name);
+                $ext = last($extArray);
+
+                $newImageName = $category->id . '.' . $ext;
+                $sPath = public_path() . '/temp/' . $tempImage->name;
+                $dPath = public_path() . '/uploads/category/' . $newImageName;
+                File::copy($sPath, $dPath);
+
+                // $dPath = public_path() . '/uploads/category/thumb/' . $newImageName;
+                // $img = Image::make($sPath);
+                // $img->resize(450, 600);
+                // $img->save($dPath);
+
+                $category->image = $newImageName;
+                $category->save();
+            }
+            Session::flash('success', 'Category added successfully');
+            return response()->json([
+                'status' => true,
+                'message' => 'Category added successfully.'
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+    }
+
+    public function edit($id)
+    {
+    }
+
+    public function update(Request $request, $id)
+    {
+    }
+
+    public function destroy($id)
+    {
+    }
+}
